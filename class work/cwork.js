a37f54dd8670bd4d4966f3fbd1d6641e1590a5b8
Shiva Chaudhary <shiva.chaudhary.0107@gmail.com>
2026-02-03 14:50:33 +0530

    assgn done

--- class work/cwork.js
+++ class work/cwork.js
@@ -61,5 +61,5 @@ const server=http.createServer((req,res)=>{
         res.end(JSON.stringify(response));
 });
 server.listen(3000,()=>{
-    console.log("server is started");
+    console.log("server is startd");
 });
